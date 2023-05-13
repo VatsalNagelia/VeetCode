f99b25ced09dbe959283f9665db717f26ee28fc4
VatsalNagelia <vatsalnagelia17@gmail.com>
2023-05-14 02:36:01 +0530

    Connect problems page to backend

--- client/src/Components/ProblemsPage/ProblemsPage.jsx
+++ client/src/Components/ProblemsPage/ProblemsPage.jsx
@@ -1,60 +1,66 @@
-import React , { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import "./ProblemsPage.css"
 
 
-const ProblemsPage = ({problems}) => {
-  const [CodeSeg, setCodeSeg] = useState("") ;
-  const { pid } = useParams() ;
-  const cleanId = pid.substring(1) ;
+const ProblemsPage = ({ problems }) => {
+  const [CodeSeg, setCodeSeg] = useState("");
+  const { pid } = useParams();
+  const cleanId = pid.substring(1);
+  const [problem, setProblem] = useState(null)
 
-  // console.log(cleanId) ;
+  const init = async () => {
+    const response = await fetch("http://localhost:3000/problems/" + cleanId, {
+      method: "GET",
+    })
+    const json = await response.json()
+    setProblem(json.problem)
+  }
 
-  const found = problems.find((prob)=>{
-    return prob.problemId===cleanId;
-  })
-
+  useEffect(() => {
+    init();
+  }, [])
 
   const handleKey = (event) => {
-    if (event.key == "Tab"){
-      event.preventDefault() ;
-      const { selectionStart , selectionEnd , value } = event.target ;
-      const val = value.substring(0,selectionStart) + "\t" + value.substring(selectionStart) ;
+    if (event.key == "Tab") {
+      event.preventDefault();
+      const { selectionStart, selectionEnd, value } = event.target;
+      const val = value.substring(0, selectionStart) + "\t" + value.substring(selectionStart);
       event.target.value = val;
-      event.target.selectionStart = event.target.selectionEnd = selectionStart+1;
+      event.target.selectionStart = event.target.selectionEnd = selectionStart + 1;
     }
-    setCodeSeg(event.value) ;
+    setCodeSeg(event.value);
   }
 
   return (
     <div>
 
       {
-        found? (
+        problem ? (
           <div id="problempage" className='flex-row'>
             <div className="ques">
-              <h1>{found.title}</h1>
+              <h1>{problem.title}</h1>
               <h5>Description</h5>
-              <p>{found.description}</p>
-              <code>Input : {found.exampleIn}</code>
-              <code>Output : {found.exampleOut}</code>
+              <p>{problem.description}</p>
+              <code>Input : {problem.exampleIn}</code>
+              <code>Output : {problem.exampleOut}</code>
             </div>
             <div className="code">
               <h1>Code Here</h1>
               <form className='code-form' method="post" action='/runprogram' >
-                <textarea name="SolvedCode" onKeyDown={ (event) => handleKey(event) }></textarea>
+                <textarea name="SolvedCode" onKeyDown={(event) => handleKey(event)}></textarea>
                 <button type="submit" id="test">TestCode</button>
                 <button type="submit" id="submit">SubmitCode</button>
               </form>
             </div>
           </div>
         ) :
-        (<div>The searched Question Doesn't exist</div>)
+          (<div>The searched Question Doesn't exist</div>)
       }
 
     </div>
-    
+
   )
 }
 
